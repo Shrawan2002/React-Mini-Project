cf53b8af0f62041ad983b27bbc4d27bc17f61a10
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-26 09:53:17 +0530

    initial files

--- Acitivity4/TodoApp/src/Todo.jsx
+++ Acitivity4/TodoApp/src/Todo.jsx
@@ -3,14 +3,30 @@ import { useState } from "react"
 export default function Todo(){
     let [todo,setTodo] = useState(["hello"]);
     let[newTodo,setNewTodo] = useState("");
+    let handleOnchange = (event)=>{
+        // console.log(event.target.value);
+        setNewTodo(event.target.value);
+    }
+
+    let handleButton = ()=>{
+        setTodo((todo)=>{
+            return[...todo,newTodo];
+        })
+    }
     return(
         <div>
-            <input type="text" placeholder="enter task" value={newTodo} />
+            <input type="text" placeholder="enter task"  onChange={handleOnchange} value={newTodo} />
             <br></br>
             <br></br>
-            <button>Add Tasks </button>
+            <button onClick={handleButton}>Add Tasks </button>
             <ul>
-                <li>Sleep</li>
+               {
+                todo.map((todo)=>( 
+                    <li>
+                        <span>{todo}</span>
+                    </li>
+                ))
+               }
             </ul>
         </div>
     )
